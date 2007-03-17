60199e72164073b0cff8ef308f826028543f68e5
nagarkar <nagarkar@04e1e069-092a-0410-a34f-cb6152fafd12>
2007-03-17 06:17:12 +0000

    Autoversioning commit:  a non-deltaV client made a change to /drools.dotnet/drools.dotnet.examples/precompiled/helloworld/HelloWorldExample.cs

---
+++ drools.dotnet/drools.dotnet.examples/precompiled/helloworld/HelloWorldExample.cs
@@ -0,0 +1,157 @@
+/** 
+* Copyright (c) 2007, Ritu Jain, Chinmay Nagarkar and Sahi Technologies Pvt Ltd
+* All rights reserved.
+* Redistribution and use in source and binary forms, with or without
+* modification, are permitted provided that the following conditions are met:
+*
+*     * Redistributions of source code must retain the above copyright
+*       notice, this list of conditions and the following disclaimer.
+*     * Redistributions in binary form must reproduce the above copyright
+*       notice, this list of conditions and the following disclaimer in the
+*       documentation and/or other materials provided with the distribution.
+*     * Neither the name of the Sahi Technologies Pvt. Ltd./Esahi.com  nor the
+*       names of its contributors may be used to endorse or promote products
+*       derived from this software without specific prior written permission.
+*
+* THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND ANY
+* EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+* WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+* DISCLAIMED. IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE FOR ANY
+* DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+* (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+* LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+* ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+* (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+* SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+using System;
+using NUnit.Framework;
+using System.IO;
+using System.Collections;
+using System.Reflection;
+using System.Text;
+using org.drools.dotnet.compiler;
+using org.drools.dotnet.rule;
+
+
+
+namespace org.drools.dotnet.examples.precompiled
+{
+
+    [TestFixture]
+    public class HelloWorldTests
+    {
+       
+        public void TestASavePackage()
+        {
+
+
+            PackageBuilder builder = new PackageBuilder();
+            System.IO.Stream stream = Assembly.GetAssembly(this.GetType()).GetManifestResourceStream("org.drools.dotnet.examples.rules.precompiled.HelloWorld.drl");
+            builder.AddPackageFromDrl("HelloWorld.drl", stream);
+            FileStream fstream = new FileStream("C:\\helloworldPackage.pkg", FileMode.Create);
+            builder.GetPackage().Save(fstream);
+            stream.Close();
+            fstream.Close();
+
+
+        }
+       
+        public void TestBLoadPackage()
+        {
+            FileStream stream = new FileStream("C:\\helloworldPackage.pkg", FileMode.Open);
+            Package newPkg = new Package();
+            newPkg.Load(stream);
+            stream.Close();
+            PackageBuilder builder = new PackageBuilder(newPkg);
+            Package pkg = builder.GetPackage();
+            RuleBase ruleBase = RuleBaseFactory.NewRuleBase();
+            ruleBase.AddPackage(pkg);
+            WorkingMemory workingMemory = ruleBase.NewWorkingMemory();
+
+            //go !
+            Message message = new Message();
+            message.message = "Hello World";
+            message.status = Message.HELLO;
+            workingMemory.assertObject(message);
+            workingMemory.fireAllRules();
+
+
+
+        }
+
+        
+        public void TestCSaveRuleBase()
+        {
+
+
+            PackageBuilder builder = new PackageBuilder();
+            System.IO.Stream stream = Assembly.GetAssembly(this.GetType()).GetManifestResourceStream("org.drools.dotnet.examples.rules.precompiled.HelloWorld.drl");
+            builder.AddPackageFromDrl("HelloWorld.drl", stream);
+            Package pkg = builder.GetPackage();
+            RuleBase ruleBase = RuleBaseFactory.NewRuleBase();
+            ruleBase.AddPackage(pkg);
+            FileStream fstream = new FileStream("C:\\helloworldRuleBase.pkg", FileMode.Create);
+            ruleBase.Save(fstream);
+            fstream.Close();
+        }
+
+        [Test]
+        public void TestDLoadRuleBase()
+        {
+            RuleBase ruleBase = RuleBaseFactory.NewRuleBase();
+            FileStream fstream = new FileStream("C:\\helloworldRuleBase.pkg", FileMode.Open);
+            ruleBase.Load(fstream);
+            WorkingMemory workingMemory = ruleBase.NewWorkingMemory();
+
+            //go !
+            Message message = new Message();
+            message.message = "Hello World";
+            message.status = Message.HELLO;
+            workingMemory.assertObject(message);
+            workingMemory.fireAllRules();
+
+        }
+ 
+    }
+
+    
+    public class Message
+    {
+        
+        public static int HELLO = 0;
+        
+        public static int GOODBYE = 1;
+
+        
+        private string messageStr;
+
+        
+        private int statusInt;
+
+        public string message
+        {
+            get { return messageStr; }
+            set
+            {
+                messageStr = value;
+            }
+        }
+
+
+        public int status
+        {
+            get { return statusInt; }
+            set
+            {
+                statusInt = value;
+            }
+        }
+
+        
+    }
+}
+
+    
+
+
