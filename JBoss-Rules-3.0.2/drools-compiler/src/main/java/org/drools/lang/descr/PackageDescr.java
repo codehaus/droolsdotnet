9be3fe80430defd15bf232adaeac4606fd7730ec
nagarkar <nagarkar@04e1e069-092a-0410-a34f-cb6152fafd12>
2007-09-08 03:58:48 +0000

    Autoversioning commit:  a non-deltaV client made a change to /JBoss-Rules-3.0.2/drools-compiler/src/main/java/org/drools/lang/descr/PackageDescr.java

---
+++ JBoss-Rules-3.0.2/drools-compiler/src/main/java/org/drools/lang/descr/PackageDescr.java
@@ -0,0 +1,114 @@
+package org.drools.lang.descr;
+
+/*
+ * Copyright 2005 JBoss Inc
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class PackageDescr extends PatternDescr {
+    /**
+     * 
+     */
+    private static final long serialVersionUID = 4491974850482281807L;
+    private final String name;
+    private final String documentation;
+
+    private List         imports    = Collections.EMPTY_LIST;
+    private List         attributes = Collections.EMPTY_LIST;
+    private Map          globals    = Collections.EMPTY_MAP;
+    private List         functions  = Collections.EMPTY_LIST;
+    private List         rules      = Collections.EMPTY_LIST;
+
+    public PackageDescr(final String name) {
+        this( name,
+              "" );
+    }
+
+    public PackageDescr(final String name,
+                        final String documentation) {
+        this.name = name;
+        this.documentation = documentation;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public String getDocumentation() {
+        return this.documentation;
+    }
+
+    public void addImport(final String importEntry) {
+        if ( this.imports == Collections.EMPTY_LIST ) {
+            this.imports = new ArrayList();
+        }
+        this.imports.add( importEntry );
+    }
+
+    public List getImports() {
+        return this.imports;
+    }
+
+    public void addGlobal(final String identifier,
+                          final String type) {
+        if ( this.globals == Collections.EMPTY_MAP ) {
+            this.globals = new HashMap();
+        }
+        this.globals.put( identifier,
+                          type );
+    }
+
+    public Map getGlobals() {
+        return this.globals;
+    }
+
+    public void addAttribute(final AttributeDescr attribute) {
+        if ( this.attributes == Collections.EMPTY_LIST ) {
+            this.attributes = new ArrayList();
+        }
+        this.attributes.add( attribute );
+    }
+
+    public List getAttributes() {
+        return this.attributes;
+    }
+
+    public void addFunction(final FunctionDescr function) {
+        if ( this.functions == Collections.EMPTY_LIST ) {
+            this.functions = new ArrayList( 1 );
+        }
+        this.functions.add( function );
+    }
+
+    public List getFunctions() {
+        return this.functions;
+    }
+
+    public void addRule(final RuleDescr rule) {
+        if ( this.rules == Collections.EMPTY_LIST ) {
+            this.rules = new ArrayList( 1 );
+        }
+        this.rules.add( rule );
+    }
+
+    public List getRules() {
+        return this.rules;
+    }
+}
